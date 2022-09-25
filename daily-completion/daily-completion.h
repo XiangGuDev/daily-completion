5edccee1dd0d6fefb50a63c58f7bc1490df3dd07
Yan_F <744539997@qq.com>
2022-09-25 21:41:55 +0800

    init

---
+++ daily-completion/daily-completion.h
@@ -0,0 +1,32 @@
+﻿
+// daily-completion.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CdailycompletionApp:
+// 有关此类的实现，请参阅 daily-completion.cpp
+//
+
+class CdailycompletionApp : public CWinApp
+{
+public:
+	CdailycompletionApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CdailycompletionApp theApp;
